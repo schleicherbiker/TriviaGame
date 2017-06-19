9a95631eb7f30330c5386cf4e66ab4ccc2478014
Alec Schleicher <alecs@live.unc.edu>
2017-06-19 01:37:49 -0400

    Functionality in place

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 //Questions
 var questions = [];
 var q1 = new question("Who stars in the TV series Sherlock?", ["Benedict Cumberbatch", "Benjamin Cinderbatch", "Bimpleton Cabbagepatch", "Sherlock"], 2);
-var q2 = new question("Is it 'Sex in the City' or 'Sex and the City'?", ["Sex in the City", "Sex and the City", "One in the show and the other is the movie", "Who Cares"], "Who Cares")
+var q2 = new question("Is it 'Sex in the City' or 'Sex and the City'?", ["Sex in the City", "Sex and the City", "One in the show and the other is the movie", "Who Cares"], 3);
 
 
 function question(question, answers, correctAnswer) {
@@ -11,8 +11,8 @@ function question(question, answers, correctAnswer) {
     questions.push(this);
 }
 
-//Time ()
-var time = 2000;
+//Variables
+var questionTime = 3;
 var qNum = 0;
 var correctNum = 0;
 var wrongNum = 0;
@@ -23,27 +23,26 @@ $(document).ready(function() {
     $("#start").click(function() {startGame()});
 })
 
-function startGame() {
-    startTimer();
-    
+function startGame() {  
     drawQuestion(qNum);
 }
 
-function startTimer() {
+function startTimer(time) {
     $("#time").empty();
     $("#time").text("Time Remaining: " + time);
     timer = setInterval(function() {
         time--;
         $("#time").text("Time Remaining: " + time);
         if (time <= 0) {
-            //TODO finish game
-            alert("Times up!");
-            clearInterval(timer);
+            answeredQuestion(false);
         }
     }, 1000)
 }
 
 function drawQuestion() {
+    //Start the timer for the question
+    startTimer(questionTime);
+
     //Draw the Question
     $("#questionBox").empty();
     question = $("<div>");
@@ -58,18 +57,43 @@ function drawQuestion() {
         answer.addClass("text-center answer");
         answer.text(questions[qNum].answers[i]);
         answer.data("answer", questions[qNum].answers[questions[qNum].correctAnswer]);
+        
         //On answer click...
         answer.click(function() {
-            qNum++;
-            if ($(this).text() === $(this).data("answer")) {
-                correctNum++;
-            
-            } else {
-                wrongNum++;
-                alert("you wrong");
-            }
-            drawQuestion();
+            answeredQuestion(this);
         })
+        
         $("#answerBox").append(answer);
     }
+}
+
+function answeredQuestion(question) {
+    qNum++;
+    clearInterval(timer);
+    if (question === false)
+        unansweredNum++;
+    else if ($(question).text() === $(question).data("answer"))
+        correctNum++;
+    else
+        wrongNum++;
+
+    //Draw new question or end game
+    if (qNum < questions.length)
+        drawQuestion();
+    else
+        endGame();
+}
+
+function endGame() {
+    $("#time").empty();
+    $("#questionBox").empty();
+    $("#answerBox").empty();
+    //Show right, wrong, and unanswered questions
+    right = $("<div>");
+    right.text("Right Answers: " + correctNum);
+    wrong = $("<div>");
+    wrong.text("Wrong Answers: " + wrongNum);
+    unanswered = $("<div>");
+    unanswered.text("Unanswered: " + unansweredNum);
+    $("#time").append(right, wrong, unanswered);
 }
