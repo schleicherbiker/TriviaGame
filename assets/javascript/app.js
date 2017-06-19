a32c6e7dfa1eac6230a9ade5bb04bbfa9461929f
Alec Schleicher <schleicher@3cisd.com>
2017-06-18 20:04:31 -0400

    Individual quesiton working

---
+++ assets/javascript/app.js
@@ -0,0 +1,71 @@
+//Questions
+var questions = [];
+var q1 = new question("Who stars in the TV series Sherlock?", ["Benedict Cumberbatch", "Benjamin Cinderbatch", "Bimpleton Cabbagepatch", "Sherlock"], 2);
+//var q2 = new question("Is it 'Sex in the City' or 'Sex and the City'?", ["Sex in the City", "Sex and the City", "One in the show and the other is the movie", "Who Cares"], "Who Cares")
+
+
+function question(question, answers, correctAnswer) {
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
+    questions.push(this);
+}
+
+//Time ()
+var time = 2000;
+var qNum = 0;
+var correctNum = 0;
+var wrongNum = 0;
+var unansweredNum = 0;
+
+//Start Game when button is pressed
+$(document).ready(function() {
+    $("#start").click(function() {startGame()});
+})
+
+function startGame() {
+    startTimer();
+    
+    drawQuestion(qNum);
+}
+
+function startTimer() {
+    $("#time").empty();
+    $("#time").text("Time Remaining: " + time);
+    timer = setInterval(function() {
+        time--;
+        $("#time").text("Time Remaining: " + time);
+        if (time <= 0) {
+            //TODO finish game
+            alert("Times up!");
+            clearInterval(timer);
+        }
+    }, 1000)
+}
+
+function drawQuestion(qNum) {
+    //Draw the Question
+    question = $("<div>");
+    question.addClass("question")
+    question.text(questions[qNum].question);
+    $("#questionBox").replaceWith(question);
+
+    //Draw the Answers
+    for (i=0; i<questions[qNum].answers.length; i++) {
+        answer = $("<div>");
+        answer.addClass("text-center answer");
+        answer.text(questions[qNum].answers[i]);
+        answer.data("answer", questions[qNum].answers[questions[qNum].correctAnswer]);
+        //On answer click...
+        answer.click(function() {
+            console.log($(this).text());
+            console.log($(this).data("answer"));
+            if ($(this).text() === $(this).data("answer")) {
+                correctNum++;
+            } else {
+                wrongNum++;
+            }
+        })
+        $("#answerBox").append(answer);
+    }
+}
